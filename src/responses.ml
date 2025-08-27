5e6fa29b4d4780229eaf6019453586273cf954df
Ming <mingyli34@gmail.com>
2025-08-27 14:26:18 -0400

    Move more test

--- src/responses.ml
+++ src/responses.ml
@@ -34,16 +34,4 @@ let of_csv csv =
     |> String.Map.of_alist_exn
 ;;
 
-let%expect_test "of_csv" =
-  let csv =
-    "Timestamp,Email Address,1,2,Feedback\n\
-     7/28/2025 23:56:29,abc@gmail.com,0.75,0.9,some feedback\n\
-     8/9/2025 15:27:02,def@gmail.com,0.32,0.999,more feedback"
-  in
-  let responses = of_csv csv in
-  print_s [%sexp (responses : t String.Map.t)];
-  [%expect
-    {| 
-    ((abc@gmail.com ((probabilities ((1 0.75) (2 0.9))))) 
-     (def@gmail.com ((probabilities ((1 0.32) (2 0.999))))))    |}]
-;;
+
---
+++ test/test_outcome.ml
@@ -0,0 +1,42 @@
+open! Core
+open Crystal
+
+let%expect_test "score" =
+  let outcomes : Outcome.t list = [ Pending; Yes; No ] in
+  let probabilities = [ 0.0; 0.1; 0.5; 0.9; 1.0 ] in
+  let rows =
+    List.cartesian_product outcomes probabilities
+    |> List.map ~f:(fun (outcome, probability) ->
+      outcome, probability, Outcome.score outcome ~probability)
+  in
+  let columns =
+    let c = Ascii_table.Column.create in
+    [ c "Outcome" (fun (outcome, _, _) -> Outcome.to_string outcome)
+    ; c "Probability" (fun (_, prob, _) -> Float.to_string prob)
+    ; c "Score" (fun (_, _, score_val) ->
+        if Float.is_nan score_val then "nan" else Float.to_string score_val)
+    ]
+  in
+  print_endline (Ascii_table.to_string columns rows ~bars:`Unicode);
+  [%expect
+    {|
+    ┌─────────┬─────────────┬─────────────────────┐
+    │ Outcome │ Probability │ Score               │
+    ├─────────┼─────────────┼─────────────────────┤
+    │ Pending │ 0.          │ nan                 │
+    │ Pending │ 0.1         │ nan                 │
+    │ Pending │ 0.5         │ nan                 │
+    │ Pending │ 0.9         │ nan                 │
+    │ Pending │ 1.          │ nan                 │
+    │ Yes     │ 0.          │ -inf                │
+    │ Yes     │ 0.1         │ -1.6094379124341    │
+    │ Yes     │ 0.5         │ 0.                  │
+    │ Yes     │ 0.9         │ 0.58778666490211906 │
+    │ Yes     │ 1.          │ 0.69314718055994529 │
+    │ No      │ 0.          │ 0.69314718055994529 │
+    │ No      │ 0.1         │ 0.58778666490211906 │
+    │ No      │ 0.5         │ 0.                  │
+    │ No      │ 0.9         │ -1.6094379124341005 │
+    │ No      │ 1.          │ -inf                │
+    └─────────┴─────────────┴─────────────────────┘ |}]
+;;
