456d4855ce5712a4f5d7c8aab84fd40f3df97191
Ming <mingyli34@gmail.com>
2025-09-02 18:03:27 -0400

    Migrate page to bonsai

--- src/plotly/plotly.mli
+++ src/plotly/plotly.mli
@@ -1,18 +1,6 @@
 open Js_of_ocaml
 
-type t =
-  < newPlot :
-      Dom_html.divElement Js.t
-      -> Data.t Js.t Js.js_array Js.t
-      -> Layout.t Js.t
-      -> Config.t Js.t
-      -> unit Js.meth
-  ; react :
-      Dom_html.divElement Js.t
-      -> Data.t Js.t Js.js_array Js.t
-      -> Layout.t Js.t
-      -> Config.t Js.t
-      -> unit Js.meth >
+type t
 
 val plotly : t Js.t
 val create : Dom_html.divElement Js.t -> Data.t list -> Layout.t -> Config.t -> unit
