d3a74d4704320f758731d80a443ba20f5d81376b
Ming <mingyli34@gmail.com>
2025-10-08 18:06:52 -0400

    Remove example Bonsai form components
    The commit removes unused example form components written in Bonsai,
    including list forms, multi-select, querybox, and textbox examples. It
    also simplifies event_id.ml by removing a redundant to_int function.

--- src/event_id.ml
+++ src/event_id.ml
@@ -2,5 +2,3 @@ open! Core
 include Int
 
 let caqti_type = Caqti_type.int
-
-let to_int t = t
--- src/event_id.mli
+++ src/event_id.mli
@@ -3,8 +3,8 @@ open! Core
 type t [@@deriving compare, sexp]
 
 val of_int : int -> t
+val to_int : t -> int
 val caqti_type : t Caqti_type.t
 val to_string : t -> string
-val to_int : t -> int
 
 include Comparable.S with type t := t
